8ed690aef0662a0c54dee0cb9bea269bbfabd4ef
joar99 <theereal_jonas@outlook.com>
2024-05-22 13:02:16 +0200

    first commit

---
+++ ComicReader/ComicReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicReaderApp.swift
+//  ComicReader
+//
+//  Created by Jonas on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ComicReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
