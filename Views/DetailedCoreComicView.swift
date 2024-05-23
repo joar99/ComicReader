c2ce9c36f6c669d691542bc1c66d04c0c25987da
joar99 <theereal_jonas@outlook.com>
2024-05-23 20:12:12 +0200

    added view to view details of a comic stored in coredata

--- Views/DetailedCoreComicView.swift
+++ Views/DetailedCoreComicView.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct DetailedCoreComicView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  
+  var comic: ComicEntity
+  
+  var body: some View {
+    ScrollView {
+      VStack {
+        Text(comic.title ?? "Unknown Title")
+          .font(.largeTitle)
+          .padding(.bottom, 10)
+        
+        releaseDateView(year: comic.year, month: comic.month, day: comic.day)
+          .padding(.bottom, 10)
+        
+        localImageView(for: comic.img)
+        
+        Text("Transcript:")
+          .font(.headline)
+          .padding(.top, 10)
+        
+        Text(comic.transcript ?? "No Transcript")
+          .padding(.bottom, 10)
+        
+        Text("Explanation:")
+          .font(.headline)
+          .padding(.top, 10)
+        
+        Text(comic.explanation ?? "No Explanation")
+        
+        Spacer()
+      }
+    }.padding()
+  }
 }
 
-#Preview {
-    DetailedCoreComicView()
+extension DetailedCoreComicView {
+  @ViewBuilder
+  private func localImageView(for imagePath: String?) -> some View {
+    if let imagePath = imagePath, let uiImage = UIImage(contentsOfFile: imagePath) {
+      Image(uiImage: uiImage)
+        .resizable()
+        .scaledToFit()
+        .frame(width: 300, height: 300)
+    } else {
+      Image(systemName: "photo")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 100, height: 100)
+        .foregroundColor(.gray)
+    }
+  }
 }
+
+
