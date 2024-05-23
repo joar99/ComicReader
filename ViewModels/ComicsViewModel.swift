1d560377c2e515887eac3133f127f55c03ebe808
joar99 <theereal_jonas@outlook.com>
2024-05-23 23:25:20 +0200

    moved mock viewmodel into separate file

--- ViewModels/ComicsViewModel.swift
+++ ViewModels/ComicsViewModel.swift
@@ -177,15 +177,3 @@ class ComicsViewModel: ObservableObject {
   }
   
 }
-
-
-extension ComicsViewModel {
-  static var preview: ComicsViewModel {
-    let model = ComicsViewModel(dataManager: ComicDataManager(), container: NSPersistentContainer(name: ""))
-    model.comicList = [
-      Comic(month: "7", num: 615, link: "", year: "2009", news: "", safe_title: "Woodpecker", transcript: "A mock comic transcript", alt: "A mock comic alt text", img: "https://imgs.xkcd.com/comics/woodpecker.png", title: "Woodpecker", day: "24"),
-      Comic(month: "7", num: 616, link: "", year: "2009", news: "", safe_title: "Squirrel", transcript: "Another mock comic transcript", alt: "Another mock comic alt text", img: "https://imgs.xkcd.com/comics/squirrel.png", title: "Squirrel", day: "25")
-    ]
-    return model
-  }
-}
