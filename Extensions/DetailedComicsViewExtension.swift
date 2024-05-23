58ceea6a220c73392b4568f087b746293f1340d9
joar99 <theereal_jonas@outlook.com>
2024-05-23 20:18:47 +0200

    removed from extension status to make usable outside scope

--- Extensions/DetailedComicsViewExtension.swift
+++ Extensions/DetailedComicsViewExtension.swift
@@ -7,25 +7,25 @@
 
 import SwiftUI
 
-extension DetailedComicView {
-  func fullDateString(year: String, month: String, day: String) -> String? {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyy MM dd"
-    if let date = dateFormatter.date(from: "\(year) \(month) \(day)") {
-      dateFormatter.dateFormat = "EEEE, MMMM dd, yyyy"
-      return dateFormatter.string(from: date)
-    }
-    return nil
+
+func fullDateString(year: String, month: String, day: String) -> String? {
+  let dateFormatter = DateFormatter()
+  dateFormatter.dateFormat = "yyyy MM dd"
+  if let date = dateFormatter.date(from: "\(year) \(month) \(day)") {
+    dateFormatter.dateFormat = "EEEE, MMMM dd, yyyy"
+    return dateFormatter.string(from: date)
   }
-  
-  @ViewBuilder
-  func releaseDateView(year: String?, month: String?, day: String?) -> some View {
-    if let year = year, let month = month, let day = day,
-       let dateString = fullDateString(year: year, month: month, day: day) {
-      Text("Release Date: \(dateString)")
-    } else {
-      Text("Release Date: Unknown")
-    }
+  return nil
+}
+
+@ViewBuilder
+func releaseDateView(year: String?, month: String?, day: String?) -> some View {
+  if let year = year, let month = month, let day = day,
+     let dateString = fullDateString(year: year, month: month, day: day) {
+    Text("Release Date: \(dateString)")
+  } else {
+    Text("Release Date: Unknown")
   }
 }
+
 
