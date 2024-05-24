7e0002b7c3fa5450c7cc08f3ae60136fc02eae15
joar99 <theereal_jonas@outlook.com>
2024-05-24 14:03:03 +0200

    removed interaction with coredata in this viewmodel

--- ViewModels/ComicsViewModel.swift
+++ ViewModels/ComicsViewModel.swift
@@ -34,7 +34,6 @@ class ComicsViewModel: ObservableObject {
   }
   
   func initializeComics() async {
-    fetchComics()
     await loadLatestComic()
     await loadComics()
   }
@@ -74,109 +73,5 @@ class ComicsViewModel: ObservableObject {
     }
   }
   
-  //CORE DATA INTERACTIONS
-  
-  func fetchComics() {
-    let request = NSFetchRequest<ComicEntity>(entityName: "ComicEntity")
-    
-    do {
-      let result = try container.viewContext.fetch(request)
-      self.savedComics = result
-      self.favoriteComicIDs = Set(result.compactMap { Int($0.id)})
-    } catch {
-      print("Error fetching entities \(error)")
-    }
-  }
-  
-  func toggleFavorite(for index: Int) async {
-    guard let isCurrentlyFavorite = comicList[index].isFavorite else {
-      comicList[index].isFavorite = true
-      await addComic(comic: comicList[index])
-      return
-    }
-    if isCurrentlyFavorite {
-      comicList[index].isFavorite = false
-      removeComic(id: comicList[index].id)
-    } else {
-      comicList[index].isFavorite = true
-      await addComic(comic: comicList[index])
-    }
-  }
-  
-  func addComic(comic: Comic) async {
-    print("Calling addcomic")
-    let context = container.viewContext
-    let newComic = ComicEntity(context: context)
-    
-    newComic.id = Int64(comic.id)
-    newComic.title = comic.title
-    newComic.year = comic.year
-    newComic.month = comic.month
-    newComic.day = comic.day
-    newComic.transcript = comic.transcript
-    newComic.alt = comic.alt
-    
-    if let explanation = comic.explanation {
-      newComic.explanation = explanation
-    } else {
-      do {
-        let parser = HTMLParser()
-        let htmlString = try await dataManager.fetchExplanationHTML(for: comic.id)
-        let explanation = try parser.parseExplanation(from: htmlString)
-        newComic.explanation = explanation
-      } catch {
-        print("Error fetching HTML string \(error)")
-      }
-    }
-    
-    do {
-      if let imagePath = try await dataManager.downloadImage(from: comic.img, withID: comic.id) {
-        newComic.img = imagePath
-      } else {
-        print("Failed to download image for comic \(comic.id)")
-        newComic.img = nil
-      }
-    } catch {
-      print("Error downloading image for comic \(comic.id): \(error)")
-    }
-    
-    saveData()
-  }
-  
-  func removeComic(id: Int) {
-    let request = NSFetchRequest<ComicEntity>(entityName: "ComicEntity")
-    request.predicate = NSPredicate(format: "id == %d", id)
-    
-    do {
-      let results = try container.viewContext.fetch(request)
-      if let existingComic = results.first {
-        if let imagePath = existingComic.img {
-          deleteImageFile(atPath: imagePath)
-        }
-        container.viewContext.delete(existingComic)
-        saveData()
-      }
-    } catch {
-      print("Error deleting comic: \(error)")
-    }
-  }
-  
-  func deleteImageFile(atPath path: String) {
-    let fileManager = FileManager.default
-    do {
-      try fileManager.removeItem(atPath: path)
-      print("Successfully deleted image at path \(path).")
-    } catch {
-      print("Failed to delete image at path \(path): \(error).")
-    }
-  }
-  
-  func saveData() {
-    do {
-      try container.viewContext.save()
-    } catch {
-      print("Error saving container \(error)")
-    }
-  }
   
 }
---
+++ ViewModels/CoreComicViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CoreComicViewModel.swift
+//  ComicReader
+//
+//  Created by Jonas on 24/05/2024.
+//
+
+import Foundation
