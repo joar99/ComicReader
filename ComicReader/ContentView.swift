5181096eacad338ace900b70f634d3217cc1f560
joar99 <theereal_jonas@outlook.com>
2024-05-24 14:41:49 +0200

    set up store, 2 separate viewmodels, and injections and initialized subscriber in coremodel

--- ComicReader/ContentView.swift
+++ ComicReader/ContentView.swift
@@ -10,12 +10,24 @@ import CoreData
 
 struct ContentView: View {
   
+  let dataManager: ComicDataManagerProtocol
+  let container: NSPersistentContainer
+  let favoriteStore: FavoriteComicsStore
+  
   @StateObject var comicModel: ComicsViewModel
+  @StateObject var coreModel: CoreComicViewModel
   
   init() {
-    let dataManager: ComicDataManagerProtocol = ComicDataManager()
+    let dataManager = ComicDataManager()
     let container = NSPersistentContainer(name: "ComicContainer")
-    _comicModel = StateObject(wrappedValue: ComicsViewModel(dataManager: dataManager, container: container))
+    let favoriteStore = FavoriteComicsStore()
+    
+    self.dataManager = dataManager
+    self.container = container
+    self.favoriteStore = favoriteStore
+    
+    _comicModel = StateObject(wrappedValue: ComicsViewModel(dataManager: dataManager, favoriteStore: favoriteStore))
+    _coreModel = StateObject(wrappedValue: CoreComicViewModel(dataManager: dataManager, container: container, favoriteStore: favoriteStore))
   }
   
   var body: some View {
@@ -23,11 +35,15 @@ struct ContentView: View {
       AllComicsView(comicModel: comicModel)
         .tabItem { Label("Explore Comics", systemImage: "book.fill") }
       
-      AllCoreComicsView(comicModel: comicModel)
+      AllCoreComicsView(coreModel: coreModel)
         .tabItem { Label("My Comics", systemImage: "person.fill") }
     }
     .onAppear {
-      Task {await comicModel.initializeComics()}
+      coreModel.subscribeToFavoriteToggles(publisher: comicModel.favoriteTogglePublisher)
+      Task {
+        coreModel.fetchComics()
+        await comicModel.initializeComics()
+      }
     }
   }
 }
