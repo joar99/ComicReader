f17374753c0a3c4d575a1252c9aef5ee622dbdd5
joar99 <theereal_jonas@outlook.com>
2024-05-24 14:40:56 +0200

    updated mock for favoritestore

--- Extensions/ComicsViewModelExtension.swift
+++ Extensions/ComicsViewModelExtension.swift
@@ -10,7 +10,7 @@ import CoreData
 
 extension ComicsViewModel {
   static var mock: ComicsViewModel {
-    let mockModel = ComicsViewModel(dataManager: ComicDataManager(), container: NSPersistentContainer(name: ""))
+    let mockModel = ComicsViewModel(dataManager: ComicDataManager(), favoriteStore: FavoriteComicsStore())
     mockModel.comicList = [
       Comic(month: "7", num: 615, link: "", year: "2009", news: "", safe_title: "Woodpecker", transcript: "A mock comic transcript", alt: "A mock comic alt text", img: "https://imgs.xkcd.com/comics/woodpecker.png", title: "Woodpecker", day: "24"),
       Comic(month: "7", num: 616, link: "", year: "2009", news: "", safe_title: "Squirrel", transcript: "Another mock comic transcript", alt: "Another mock comic alt text", img: "https://imgs.xkcd.com/comics/squirrel.png", title: "Squirrel", day: "25")
